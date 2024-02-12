8b15771201c9ac47c9914a8c66a1b2b448061d57
ahmedmedht <ahmed.m.mehisen@gmail.com>
2024-02-12 06:40:34 +0200

    ahhhh

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         public static final String EXIT_COMMAND = "exit";
 
         public static void main(String[] args) {
+        
             Menu mainMenu = new MainMenu();
             mainMenu.start();
         }
